ce9965df652f22f46b146b417a335324cf0332f0
Peter <peter@Zipcoders-MacBook-Pro-6.local>
2024-07-05 16:34:16 -0400

    Finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,45 @@ public class Person {
     private String name;
     private int age;
 
+    // default has no parameters
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
-
+    // constructor age
     public Person(int age) {
+        this.age = age;
     }
 
+    //constructor with name
     public Person(String name) {
+        this.name = name;
     }
 
+    // constructor with name and age
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    // these aren't constructors, they are methods
+    // *******************************************
+
+    // use this. because it's in the class itself
     public void setName(String name) {
+        this.name = name;
     }
+
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
